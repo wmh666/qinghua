9dec1d31bb30938eb8304cc56ce16e11431555c4
wangmenghao <205251076@qq.com>
2019-09-17 15:01:36 +0800

    bug

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Model\Import;
 use Illuminate\Http\Request;
+use Excel;
 class ImportController extends Controller {
 
-    public function import(){
-        $list = Import::import();
-        dd($list);
+    public function import(Request $requests){
+        $file = $requests->file('file');
+        $filePath = $file->getRealPath();
+            excel::load($filePath, function($reader) {
+            $data = $reader->all();
+            dd($data);
+        });
     }
 }
